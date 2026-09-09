497bf66cd50031fde517035e9a3abd324174dd94
Vladimir Valekzhanin <valdemarcheck@yandex.ru>
2026-09-09 21:01:04 +0300

    Решить задачи листка 1 в ноутбуке Jupyter

---
+++ scripts/execute_notebook.py
@@ -0,0 +1,13 @@
+"""Выполнение ноутбука с помощью nbclient (headless)."""
+
+import sys
+
+import nbformat as nbf
+from nbclient import NotebookClient
+
+path = sys.argv[1] if len(sys.argv) > 1 else "notebooks/Листок-1.ipynb"
+nb = nbf.read(path, as_version=4)
+client = NotebookClient(nb, timeout=600, kernel_name="python3")
+client.execute()
+nbf.write(nb, path)
+print(path, "executed")
